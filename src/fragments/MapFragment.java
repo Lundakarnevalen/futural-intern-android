7ddc642e4a056c1e7ccc6b72e1331871e1ffbd15
atp10mla <atp10mla@student.lu.se>
2014-03-29 17:22:34 +0100

    add handler for sending gps-pos and get positions, add anti-spam, working correctly for test server

--- src/fragments/MapFragment.java
+++ src/fragments/MapFragment.java
@@ -15,8 +15,10 @@ import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -31,10 +33,35 @@ import com.google.gson.Gson;
 
 public class MapFragment extends LKFragment {
 
+
+	private long lastUpdate;
+
+	// Pre pic.
+	private Bitmap bmOverlay;
+
+	private static final int HANDLER_DELAY = 1800000; //30min
+	//private static final int HANDLER_DELAY = 10000; //10sec
+	private static final int GET_HANDLER_DELAY = 1800000; //30min
+	//private static final int GET_HANDLER_DELAY = 10000; //30min
+
+	private Context context;
+
+	private Handler getHandler;
+
+	private Handler handler;
+	private LocationManager locMan;
+
+	private static final int TIME_INTERVAL = 1800000; // get gps location every 30 min
+	//private static final int TIME_INTERVAL = 10000; // get gps location every 30 min
+
+	//private static final int TIME_INTERVAL = 1800000; // get gps location every 30 min
+	private static final int GPS_DISTANCE = 0; // set the distance value in meter
+
+
 	private final String token = "P6VmxzvTypzP3qb3TEW7";
-	
+
 	private final String SHARED_ID = "SHAREDID";
-	
+
 	private float maxDotSize = 40;
 	private float minDotSize = 10;
 	private float diffDotSize = maxDotSize - minDotSize;
@@ -44,14 +71,14 @@ public class MapFragment extends LKFragment {
 	private float endLatMap = (float)56.52300194685981;
 	private float diffLon = endLonMap - startLonMap;
 	private float diffLat = endLatMap - startLatMap;
-	
+
 	private int clusterId = -1; 
 	private int nbrOfPersons;
 
 	private final String key_cluster = "key_cluster_id";
 
 	private List<Position> positions;
-	
+
 	private ImageView img;
 	private boolean gpsOn = true;
 
@@ -59,31 +86,62 @@ public class MapFragment extends LKFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fr_layout_map, null);
-		
+
+		context = getContext();
+
 		ActionBar actionBar = ((ActionBarActivity) getActivity()).getSupportActionBar();
 		View root = actionBar.getCustomView();
 		RelativeLayout gpsCheckbox = (RelativeLayout) root.findViewById(R.id.gps_checkbox);	
 		gpsCheckbox.setOnClickListener(gpsCheckboxListener);
 		gpsCheckbox.setVisibility(View.VISIBLE);
 
+		se.lundakarnevalen.widget.LKTextView text = (se.lundakarnevalen.widget.LKTextView) rootView.findViewById(R.id.nbr_of);
+		//TODO change to english!
+		text.setText(nbrOfPersons+getString(R.string.map_karnevalister));
+				
 		img = (ImageView) rootView.findViewById(R.id.map_id);
-		
-		SharedPreferences prefs = getContext().getSharedPreferences(SHARED_ID, getContext().MODE_PRIVATE);
-		clusterId = prefs.getInt(key_cluster, -1); 
+		if(bmOverlay != null) {
+			((ImageView) rootView.findViewById(R.id.map_id)).setImageBitmap(bmOverlay);
+  
+		} 		
+		if(clusterId == -1) {
+			SharedPreferences prefs = getContext().getSharedPreferences(SHARED_ID, Context.MODE_PRIVATE);
+			clusterId = prefs.getInt(key_cluster, -1); 
+		}
 		Log.d("CLUSTER_ID",""+clusterId);
-		
-		//TODO 
-		// Call updatePositions(positions) instead.
 
-		//TODO 
-		// Call every ??
-		//sendPosition();
-		//getAndUpdatePositions();
-		
-		//TODO Do this in background..
-		// If we dont want to update the position we do updatePositions(positions) in background...
-		sendPosition();
-		getPositions();
+		if(handler == null) {
+			handler = new Handler();
+
+			handler.postDelayed(new Runnable() {
+				public void run() {
+					sendPosition();
+					handler.postDelayed(this, HANDLER_DELAY);
+				}
+			}, HANDLER_DELAY); //START_HANDLER_DELAY
+		}
+
+		if(getHandler == null) {
+			getHandler = new Handler();
+
+			getHandler.postDelayed(new Runnable() {
+				public void run() {
+					if(isVisible()) {
+						getPositions();
+					}
+					getHandler.postDelayed(this, GET_HANDLER_DELAY);
+				}
+			}, GET_HANDLER_DELAY); //START_HANDLER_DELAY
+		}
+		if(positions == null || positions.size() == 0) {
+			getPositions();
+		} else if(System.currentTimeMillis() - lastUpdate > 10000){ //10 sec spam control. 
+			Log.d("Spam control","no spam");
+			getPositions();	
+		} else {
+			Log.d("Spam control","SPAM");
+		}
+		lastUpdate = System.currentTimeMillis();
 		return rootView;
 	}
 
@@ -92,6 +150,7 @@ public class MapFragment extends LKFragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		setTitle(getString(R.string.karta));
+	
 		//TODO
 		// Fix both eng and swe.
 	}
@@ -102,56 +161,46 @@ public class MapFragment extends LKFragment {
 	 */
 	private boolean sendPosition() {
 		Log.d("GPS on:",""+gpsOn);
-		if(gpsOn) {
-			float lng;
-			float lat;
-			LocationManager mlocManager = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE); 
-			Location location = null;
-			if(mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-				location= mlocManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-				if(location!=null) {
-					lng = (float)location.getLongitude();
-					lat = (float)location.getLatitude();
-					Log.d("Find GPS_position",lng +" "+lat);
-				} else {
-					if(mlocManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-						location= mlocManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-						if(location!=null){
-							lng = (float)location.getLongitude();
-							lat = (float)location.getLatitude();
-							Log.d("Find Network_position",lng +" "+lat);
-						} else {
-							Log.d("No GPS or Network position","FAIL1");
-							return false;
-						}
-					} else {
-						Log.d("No GPS or Network position","FAIL2");
-						return false;
-					}
-				}
-			} else {
-				if(mlocManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-					location= mlocManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-					if(location!=null){ 
-						lng = (float)location.getLongitude();
-						lat = (float)location.getLatitude();
-						Log.d("Find Network_position2",lng +" "+lat);
-					} else {
-						Log.d("No GPS or Network position","FAIL3");
-						return false;
-					}
-				} else {
-					Log.d("No GPS or Network position","FAIL4");
-					return false;
-				}
-			}
-			postPosition(lat, lng);
-
-			return true;
-		} else {
-			Log.d("Don´t want to send position.","FAIL5");
-			return false;
+		if(locMan==null) {
+			locMan = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
 		}
+		// Only turn off get position with GPS. Ok with network...
+		if(gpsOn && locMan.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+			//if(isLocationListener){
+			locMan.requestLocationUpdates(LocationManager.GPS_PROVIDER, TIME_INTERVAL, GPS_DISTANCE, PositionListener);
+			Log.d("Updateing GPS!","Update");
+			//}
+		} else {
+			Log.d("GPS off","Avstängd GPS");
+			if(locMan.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+				locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, TIME_INTERVAL, GPS_DISTANCE, PositionListener);
+				Log.d("Updateing Position with network!","Update");
+			}
+		}
+
+		float lng;
+		float lat;
+		Location location = null;
+		location= locMan.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		if(location!=null) {
+			lng = (float)location.getLongitude();
+			lat = (float)location.getLatitude();
+			Log.d("Find GPS_position",lng +" "+lat);
+		} else {
+			location= locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+			if(location!=null){
+				lng = (float)location.getLongitude();
+				lat = (float)location.getLatitude();
+				Log.d("Find Network_position",lng +" "+lat);
+			} else {
+				Log.d("No GPS or Network position","FAIL1");
+				return false;
+			}
+		} 
+
+		postPosition(lat, lng);
+		return true;
+
 	}
 
 	private void updatePositions(List<Position> positions) {
@@ -163,7 +212,7 @@ public class MapFragment extends LKFragment {
 			Bitmap mapBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.map_skane);
 
 			// Create an overlay bitmap
-			Bitmap bmOverlay = Bitmap.createBitmap(mapBitmap.getWidth(), mapBitmap.getHeight(), mapBitmap.getConfig());
+			bmOverlay = Bitmap.createBitmap(mapBitmap.getWidth(), mapBitmap.getHeight(), mapBitmap.getConfig());
 			Canvas canvas = new Canvas();
 			canvas.setBitmap(bmOverlay);
 			canvas.drawBitmap(mapBitmap, new Matrix(), null);
@@ -213,7 +262,7 @@ public class MapFragment extends LKFragment {
 		newPositions.add(new Position((float)55.374660,(float)13.191007,17));
 		newPositions.add(new Position((float)55.374660,(float)13.391007,40));
 		newPositions.add(new Position((float)55.934660,(float)13.951007,10));
-		*/
+		 */
 		updatePositions(positions);
 
 	}
@@ -221,24 +270,25 @@ public class MapFragment extends LKFragment {
 	private void getPositions() {
 		// TODO
 		// TOKEN ???		
-		LKRemote remote = new LKRemote(getContext(), new GetListener());
+		LKRemote remote = new LKRemote(context, new GetListener());
 		remote.requestServerForText("api/clusters?token=" + token, "", LKRemote.RequestType.GET, false);
 	}
 
 	private void postPosition(float lat, float lng) {
 		Gson g =  new Gson();
-		
+
 		MapPost mp = new MapPost(lat,lng,token);	
 		String js = g.toJson(mp);
 		Log.d("Send json: ",""+js);
-	
+
 		// TODO
 		// TOKEN ???
 		Log.d("Cluster id before post: ",""+clusterId);
-		LKRemote remote = new LKRemote(getContext(), new PostListener());
+		LKRemote remote = new LKRemote(context, new PostListener());
+
 		if(clusterId == -1) {
 			remote.requestServerForText("api/clusters", js, LKRemote.RequestType.POST, false);			
-		}else {
+		}else {	
 			// Står fel i mail, ska vara put istället för post....
 			remote.requestServerForText("api/clusters"+"/"+clusterId, js, LKRemote.RequestType.PUT, false);			
 		}
@@ -266,7 +316,7 @@ public class MapFragment extends LKFragment {
 				return;
 			}
 			Log.d("GetListener get result: ", result);
-			
+
 			Gson gson = new Gson();
 			MapGet checker = gson.fromJson(result, MapGet.class);
 
@@ -275,6 +325,7 @@ public class MapFragment extends LKFragment {
 				Log.d("GetListener sucess:","true, size:"+clusters.clusters.size());
 				positions = clusters.clusters;
 				updatePositions(clusters.clusters);
+
 			} else {
 				Log.d("GetListener sucess:","false");
 			}
@@ -291,7 +342,7 @@ public class MapFragment extends LKFragment {
 			}
 
 			Log.d("PostListener get result: ", result);
-			
+
 			Gson gson = new Gson();
 			ResultPost res = gson.fromJson(result, ResultPost.class);
 
@@ -299,7 +350,7 @@ public class MapFragment extends LKFragment {
 				Log.d("PostListener sucess:","true, clusterId:"+clusterId);
 				if(clusterId != res.cluster_id) {
 					clusterId = res.cluster_id;
-					SharedPreferences prefs = getContext().getSharedPreferences(SHARED_ID, getContext().MODE_PRIVATE);
+					SharedPreferences prefs = getContext().getSharedPreferences(SHARED_ID, Context.MODE_PRIVATE);
 					prefs.edit().putInt(key_cluster, clusterId).commit();
 					Log.d("Update clusterId to:",""+clusterId);					
 				}
@@ -309,22 +360,17 @@ public class MapFragment extends LKFragment {
 			}
 		}
 	}
-	
+
 	private class Position {
 		private float lat;
 		private float lng;
 		private int quantity;
-		public Position(float lat, float lng, int size) {
-			this.lat = lat;
-			this.lng = lng;
-			this.quantity = size;
-		}
 	}
 
 	private class Clusters {
 		private List<Position> clusters;
 	}
-	
+
 	private class ResultPost {
 		private boolean success;
 		private int cluster_id;
@@ -350,4 +396,23 @@ public class MapFragment extends LKFragment {
 		}
 		return bestLocation;
 	}
+
+
+
+
+	private LocationListener PositionListener = new LocationListener(){
+		public void onLocationChanged(Location location) {
+			// update location
+			locMan.removeUpdates(PositionListener); // remove this listener
+		}
+
+		public void onProviderDisabled(String provider) {
+		}
+
+		public void onProviderEnabled(String provider) {
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+	};
 }
