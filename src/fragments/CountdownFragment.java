4a1ef6af1082e182cb25fae0d271f1a526023ae2
AugustRansnas <august.ransnas@gmail.com>
2014-04-02 21:10:58 +0200

    added asynctask for multiple countdowntimers

--- src/fragments/CountdownFragment.java
+++ src/fragments/CountdownFragment.java
@@ -2,17 +2,9 @@ package fragments;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
-
 import se.lundakarnevalen.android.R;
-import android.R.anim;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
@@ -21,18 +13,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class CountdownFragment extends LKFragment {
-	
-	TextView tv;
-	long diff;
+
+	TextView tvKarneval;
+	TextView tvKarnelan;
+	long diffKarneVad;
 	private String antonsTestHAHAHAAHA;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Change the layout
-		View rootView = inflater.inflate(R.layout.fr_layout_countdown,null);
-		tv = (TextView) rootView.findViewById(R.id.tvCountDown);
-		//new DrawingTheCloud(this.getContext());
+		View rootView = inflater.inflate(R.layout.fr_layout_countdown, null);
+		tvKarneval = (TextView) rootView.findViewById(R.id.tvKarneval);
+		tvKarnelan = (TextView) rootView.findViewById(R.id.tvKarneLan);
 		return rootView;
 	}
 
@@ -40,79 +33,96 @@ public class CountdownFragment extends LKFragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		setTitle("Countdown");
+		Typeface tf = Typeface.createFromAsset(getActivity().getAssets(),"fonts/Roboto-Bold.ttf");
+		tvKarneval.setTypeface(tf);
 		
-		
-		Typeface tf = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-Bold.ttf");
-		tv.setTypeface(tf);
-		Date karneval = new GregorianCalendar(2014, 4, 16, 14, 0, 0).getTime();
+		Date karneVal = new GregorianCalendar(2014, 4, 16, 14, 0, 0).getTime();
+		Date karneLan = new GregorianCalendar(2014, 4, 16, 14, 0, 0).getTime();
+		Date preKarneval = new GregorianCalendar(2014, 3, 15, 11, 0, 0).getTime();
+		Date karneBeer = new GregorianCalendar(2014, 3, 15, 11, 0, 0).getTime();
+		Date postKarneval = new GregorianCalendar(2014, 3, 15, 11, 0, 0).getTime();
 		Date today = new Date();
-
-		diff = karneval.getTime() - today.getTime();
-
-		new CountDownTimer(diff, 1000) {
-
-			public void onTick(long diff) {
-				long seconds = diff / 1000;
-				long minutes = seconds / 60;
-				long hours = minutes / 60;
-				long days = hours / 24;
-
-				seconds = seconds % 60;
-				minutes = minutes % 60;
-				hours = hours % 24;
-
-				tv.setText(String.format("%02d:%02d:%02d:%02d", days, hours,
-						minutes, seconds));
-			}
-
-			public void onFinish() {
-				tv.setText("done!");
-			}
-		}.start();
 		
+		long diffKarneval = karneVal.getTime() - today.getTime();
+		long diffKarneVad = karneLan.getTime() - today.getTime();
 		
+		CountDownTask countDownTask = new CountDownTask();
+		countDownTask.execute(diffKarneval,diffKarneVad);
 	}
-	
-	/*public class DrawingTheCloud extends View{
-		
-		int x ,y;
-		Bitmap cloud;
-		int color = R.color.light_blue;
 
-		public DrawingTheCloud(Context context) {
-			super(context);
-			// TODO Auto-generated constructor stub
-			tv.setText("asdas");
-			cloud = BitmapFactory.decodeResource(getResources(),R.drawable.moln1);
-			x = 0;
-			y = 0;
-			invalidate();
-		
+	private void displayProgressKarneval(String countDownMessage) {
+		tvKarneval.setText(countDownMessage);
+	}
+	private void displayProgressKarnelan(String countDownMessageVad) {
+		tvKarnelan.setText(countDownMessageVad);
+	}
+
+	private class CountDownTask extends AsyncTask<Long, Long, Long> {
+
+		@Override
+		protected Long doInBackground(Long... params) {
+
+			final long diffKarneval = params[0];
+			final long diffKarneLan = params[1];
+			publishProgress(diffKarneval,diffKarneLan);
+			return diffKarneval;
+		}
+		@Override
+		protected void onProgressUpdate(Long... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+			long diffKarneval = values[0];
+			long diffKarneLan = values[1];
+			
+			new CountDownTimer(diffKarneval, 1000) {
+				@Override
+				public void onTick(long diffKarneval) {
+					// TODO Auto-generated method stub
+					long seconds = diffKarneval / 1000;
+					long minutes = seconds / 60;
+					long hours = minutes / 60;
+					long days = hours / 24;
+					seconds = seconds % 60;
+					minutes = minutes % 60;
+					hours = hours % 24;
+					String countDownMessage = String.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds);
+					displayProgressKarneval(countDownMessage);
+				}
+				@Override
+				public void onFinish() {
+					// TODO Auto-generated method stub
+					tvKarneval.setText("Done");
+				}
+			}.start();
+			
+			new CountDownTimer(diffKarneLan, 1000) {
+				@Override
+				public void onTick(long diffKarneLan) {
+					// TODO Auto-generated method stub
+					long seconds = diffKarneLan / 1000;
+					long minutes = seconds / 60;
+					long hours = minutes / 60;
+					long days = hours / 24;
+					seconds = seconds % 60;
+					minutes = minutes % 60;
+					hours = hours % 24;
+					String countDownMessageVad = String.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds);
+					displayProgressKarnelan(countDownMessageVad);
+				}
+				
+				@Override
+				public void onFinish() {
+					// TODO Auto-generated method stub
+					tvKarnelan.setText("Done");
+				}
+			}.start();
+			
 		}
 
 		@Override
-		protected void onDraw(Canvas canvas) {
+		protected void onPostExecute(Long result) {
 			// TODO Auto-generated method stub
-			super.onDraw(canvas);
-			tv.setText("grsd");
-			Rect ourRect = new Rect();
-			ourRect.set(0, 0, canvas.getWidth(), canvas.getHeight()/2);
-			Paint background = new Paint();
-			background.setColor(color);
-			background.setStyle(Paint.Style.FILL);
-			canvas.drawRect(ourRect, background);
-			if(x < canvas.getWidth()){
-				x +=10;
-			}else{
-				x = 0;
-			}
-			if(y<canvas.getHeight()){
-				y +=10;
-			}else{
-				y = 0;
-			}
-			canvas.drawBitmap(cloud, x, y, new Paint());
-			invalidate();
-		}	
-	}*/
+			super.onPostExecute(result);
+		}
+	}
 }
